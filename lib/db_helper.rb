44381401aca279a47fbecbcf4a7e451cd2d0de16
David Coleman <colemandavid55@gmail.com>
2015-02-04 15:45:34 -0600

    establish db_helper main functionality

--- lib/db_helper.rb
+++ lib/db_helper.rb
@@ -15,8 +15,42 @@ module ListMore
           );
       CREATE TABLE IF NOT EXISTS lists(
           id SERIAL PRIMARY KEY
-          , 
+          , name VARCHAR
+          , user_id REFERENCES users(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+          );
+      CREATE TABLE IF NOT EXISTS items(
+          id SERIAL PRIMARY KEY
+          , content TEXT
+          , user_id REFERENCES users(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+          , list_id REFERENCES lists(id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+          );
+      SQL
+    end
 
-        )
+    def self.clear_tables db
+      db.exec <<-SQL
+        DELETE FROM users;
+        DELETE FROM lists;
+        DELETE FROM items;
+      SQL
+    end
+
+    def self.drop_tables db
+      db.exec <<-SQL
+        DROP TABLE ;
+        DROP TABLE ;
+        DROP TABLE ;
+      SQL
+    end
+
+    def self.seed_tables db
+      db.exec <<-SQL
+
       SQL
     end
