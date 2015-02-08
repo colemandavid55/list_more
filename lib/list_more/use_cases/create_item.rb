2983d8bb695161646bd2ab35067f16ea94e0dbd9
David Coleman <colemandavid55@gmail.com>
2015-02-08 15:40:49 -0600

    add use case to create an item

---
+++ lib/list_more/use_cases/create_item.rb
@@ -0,0 +1,14 @@
+module ListMore
+  class CreateItem < UseCase
+
+    def run params
+      user = ListMore.users_repo.find_by_token params['token']
+      unless user
+        return failure "User does not have a session id"
+      end
+      item = ListMore::Entities::Item.new({:content => params['content'], :user_id = user.id, :list_id = params['list_id']})
+      success
+    end
+
+  end
+end
