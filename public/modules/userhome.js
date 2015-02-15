83e2547954f9198889eed4e9c39a3dfe42b04b00
David Coleman <colemandavid55@gmail.com>
2015-02-15 15:19:14 -0600

    render a user list

--- public/modules/userhome.js
+++ public/modules/userhome.js
@@ -1,5 +1,9 @@
 var UserHome = {}
 
+UserHome.vm = {
+  users: m.request({method: "GET", url: "/users", background: true, initialValue: []})
+}
+
 
 UserHome.controller = function () {
 	var ctrl = {}
@@ -7,11 +11,33 @@ UserHome.controller = function () {
   ctrl.logOut = function (e) {
     localStorage.clear()
   }
+
+  // ctrl.getUsers = function () {
+  //   return {
+  //     users: m.request({method: "GET", url: "/users", background: true, initialValue: []}).then(
+  //     function (response) {
+  //       console.log("Success",response)
+  //       m.redraw()
+  //       return response.users
+  //     },
+  //     function (error) {
+  //       console.log("Error",error)
+  //       return error
+  //     }
+  //   )}
+  // }
 	return ctrl
 }
 
 UserHome.view = function (ctrl) {
-	return m('.userpage', [m('h1', "this is the users homepage"),
-  m("a[href='/']", {config: m.route, onclick: ctrl.logOut}, "Log Out")
-  ])
+	return m('.users', [
+    m('h1', "this is the users homepage"),
+    UserHome.vm.users().users.map(userView),
+    m("a[href='/']", {config: m.route, onclick: ctrl.logOut}, "Log Out")
+    ]
+  )
+
+  function userView (user) {
+    return m('div.user', {data: user}, user.username)
+  }
 }
