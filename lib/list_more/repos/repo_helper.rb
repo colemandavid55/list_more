2ab317b8edf19f69dac21f6556f5106e2c01f6eb
David Coleman <colemandavid55@gmail.com>
2015-02-18 17:19:34 -0600

    Update repo helper to account for the difference between a dev and production environment when making a connection to the database.

--- lib/list_more/repos/repo_helper.rb
+++ lib/list_more/repos/repo_helper.rb
@@ -3,7 +3,25 @@ module ListMore
     attr_reader :db
 
     def initialize dbname
-      @db = PG.connect(host: 'localhost', dbname: dbname)
+      db_data = if ENV['RACK_ENV'] == 'production'
+        parse_rds_string
+      else
+        {host: 'localhost', dbname: dbname}
+      end
+
+      @db = PG.connect(db_data)
+    end
+
+    def parse_rds_string
+      rds_string = ENV['DATABASE_URL']
+      data = rds_string.split(/\/|:|@/)
+      {
+        host: data[5],
+        port: data[6],
+        user: data[3],
+        password: data[4],
+        dbname: data.last
+      }
     end
 
   end
